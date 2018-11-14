cdbba2c57d00361fa70b97ad6d022f1217c83d2a
Andrey Sushenkov <blognat91@gmail.com>
2018-11-14 14:47:18 +0300

    Fix

--- test/candleAndPredictionPrepareTest.spec.js
+++ test/candleAndPredictionPrepareTest.spec.js
@@ -3,7 +3,6 @@ const { candleAndPredictionPrepare } = require('../index')
 const {candles, predictions} = require('../testAssets/15m')
 
 describe('Array', async function () {
-  const timeZone = moment.tz.guess()
   const instance = new candleAndPredictionPrepare({candles, predictions, timeframe: '15m', timeZone: 'Europe/Moscow'}).run()
-  console.log(instance.r.predictions.default)
+  console.log(instance)
 });
